f8fca9663ee275d7ffa9ec7be58a277c327c1902
Rahim Kara <kara.rahim@gmail.com>
2025-03-02 17:49:45 +0100

    Commit 7 - CSS changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,11 @@ const port = 5000;
 // Serve the frontend static files
 app.use(express.static(path.join(__dirname, '../../frontend/public')));
 
+// Fallback to index.html for any other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../frontend/public', 'index.html'));
+});
+
 app.listen(port, () => {
   console.log(`Backend server running at http://localhost:${port}`);
 });
